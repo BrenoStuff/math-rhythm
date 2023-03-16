f938189e656826468aa6b676dde6fc8b6cdf93bc
FelipeTosta <tostafelipe2017@gmail.com>
2023-03-16 17:52:52 -0300

    Fiz cagada na hora de contar os erros

--- js/gerarDiv.js
+++
@@ -1,4 +0,0 @@
-export function criarDiv() {
-        var newNote = document.createElement("div");
-        newNote.classList.add("notes");
-}
--- js/script.js
+++ js/script.js
@@ -1,24 +1,20 @@
 window.onload = new function() {
 
-        let contador = 0; //c
+        let contador = 0;
         let notas = [];
+        let pontos = 1;
+
+        let btt = 0;
+        let err = 0;
+        let acc = 0;
      
         // ----------- PODE IGNORAR ESSE TRECHO ---------------- //
 
-        let botao1 = document.getElementById("key1");
-        let botao2 = document.getElementById("key2");
-        let botao3 = document.getElementById("key3");
-        let botao4 = document.getElementById("key4");
+        let botao = document.getElementById("key1");
 
-        let posKey1 = botao1.getBoundingClientRect();
-        let posKey2 = botao2.getBoundingClientRect();
-        let posKey3 = botao3.getBoundingClientRect();
-        let posKey4 = botao4.getBoundingClientRect();
+        // ----------------------------------------------------- //
 
-        console.log(posKey1);
-        console.log(posKey2);
-        console.log(posKey3);
-        console.log(posKey4);
+        let posKey = botao.getBoundingClientRect();
 
         // ----------------------------------------------------- //
 
@@ -28,36 +24,100 @@ window.onload = new function() {
                 newNote.classList.add("beat");
                 
                 let i = Math.floor(Math.random() * 4) + 1;
-                
+
                 if (contador % 2 == 0){
                         var piano = document.getElementById("noteMove" + i);
                         newNote.innerHTML = contador + ""; 
                         piano.appendChild(newNote);
                         notas.push(newNote);
-                }
+                } 
 
-                
                 console.log(i);
                 console.log(contador);
-                console.log(piano);
+                //console.log(newNote.getBoundingClientRect().y);
 
                 i = 0;
         }
 
+        var key1 = document.querySelector("#key1");
+        var key2 = document.querySelector("#key2");
+        var key3 = document.querySelector("#key3");
+        var key4 = document.querySelector("#key4");
+
+        document.addEventListener("keydown", (e) => {
+                if (e.key === "D" || e.key === "d") {
+                        btt = 1;
+                        key1.style.border = " 3px solid red";
+                        console.log("Tecla D pressionada!");
+                } else if (e.key === "F" || e.key === "f") {
+                        btt = 1;
+                        key2.style.border = " 3px solid green";
+                        console.log("Tecla F pressionada!");
+                } else if (e.key === "J" || e.key === "j") {
+                        btt = 1;
+                        key3.style.border = " 3px solid yellow";
+                        console.log("Tecla J pressionada!");
+                } else if (e.key === "K" || e.key === "k") {
+                        btt = 1;
+                        key4.style.border = " 3px solid blue";
+                        console.log("Tecla K pressionada!");
+                }
+        })
+
+        document.addEventListener("keyup", (e) => {
+                if (e.key === "D" || e.key === "d") {
+                        btt = 0;
+                        key1.style.border = " 1px solid black";
+                        console.log("Tecla D pressionada!");
+                } else if (e.key === "F" || e.key === "f") {
+                        btt = 0;
+                        key2.style.border = " 1px solid black";
+                        console.log("Tecla F pressionada!");
+                } else if (e.key === "J" || e.key === "j") {
+                        btt = 0;
+                        key3.style.border = " 1px solid black";
+                        console.log("Tecla J pressionada!");
+                } else if (e.key === "K" || e.key === "k") {
+                        btt = 0;
+                        key4.style.border = " 1px solid black";
+                        console.log("Tecla K pressionada!");
+                }
+        })
+
         function descerDiv() {
+                var acertos = document.getElementById("acertos");
+                var contagem = document.getElementById("contagem");
+                var erros = document.getElementById("erros");
+                var combos = document.getElementById("combos");
+
                 for (i = 0; i <= notas.length; i++) {
                         let nota = notas[i];
                         let posY = parseInt(nota.style.top || 0);
-                        posY += 1;
+                        posY += 3;
                         nota.style.top = posY + "px";
 
-                        if (posY >= (posKey1.y + 25)) {
+                        if (posY >= posKey.y - 40 && posY <= posKey.y - 10 && btt == 1) {
                                 nota.remove();
+                                err = err + 1;
+                                erros.innerHTML = "Erros: " + err;
+                                
+                        } else if (posY > posKey.y - 10 && posY <= posKey.y + 10 && btt == 1) {
+                                nota.remove();
+                                acc = acc + 1;
+                                acertos.innerHTML = "Acertos: " + acc;
+                        } else if (posY > posKey.y + 10 && posY <= posKey.y + 24 && btt == 1) {
+                                nota.remove();
+                                acc = acc + 1;
+                                acertos.innerHTML = "Acertos: " + acc;
+                        } else if (posY > posKey.y + 24 && btt == 0) {
+                                nota.remove();
+                                err = err + 1;
+                                erros.innerHTML = "Erros: " + err;
                         }
 
                 }
         }
-        
+
         setInterval(criarDiv, 500);
         setInterval(descerDiv, 5);
 }
