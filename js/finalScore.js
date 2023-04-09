cc8d80b0118a8c79305d7fc5fcdc958dbcbdd994
Breno Machado <brenomachado0520@gmail.com>
2023-04-08 23:31:52 -0300

    Espaçamento trocado para tab-4

--- js/finalScore.js
+++ js/finalScore.js
@@ -11,29 +11,29 @@ var cbs;
 var ctd = 1;
 
 for (i = 0; i <= pontuacoes.length - 1; i++) {
-        if (pontuacoes[i] != ",") {
-                pontos.push(pontuacoes[i]);
-                if (ctd == 4) {
-                        cbs = pontos.join("");
-                        if (pontuacoes[i] == pontuacoes.length - 1) {
-                                pontos.length = 0;
-                        }
-                }
-        } else {
-                if (ctd == 1){
-                        pts = pontos.join("");
-                        pontos.length = 0;
-                        ctd++;
-                } else if (ctd == 2) {
-                        acc = pontos.join("");
-                        pontos.length = 0;
-                        ctd++;
-                } else if (ctd == 3) {
-                        err = pontos.join("");
-                        pontos.length = 0;
-                        ctd++;
-                }
-        }
+	if (pontuacoes[i] != ",") {
+		pontos.push(pontuacoes[i]);
+		if (ctd == 4) {
+			cbs = pontos.join("");
+			if (pontuacoes[i] == pontuacoes.length - 1) {
+				pontos.length = 0;
+			}
+		}
+	} else {
+		if (ctd == 1){
+			pts = pontos.join("");
+			pontos.length = 0;
+			ctd++;
+		} else if (ctd == 2) {
+			acc = pontos.join("");
+			pontos.length = 0;
+			ctd++;
+		} else if (ctd == 3) {
+			err = pontos.join("");
+			pontos.length = 0;
+			ctd++;
+		}
+	}
 }
 
 var pontos = document.getElementById("pontuacao");
